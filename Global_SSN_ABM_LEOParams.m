a47ae8bb1bfbab95dccd26f407405a0d91f200fa
jmandara <jmandara@purdue.edu>
2023-04-29 16:57:37 -0400

    Enable LEO Independent Launch Logic
    Enable LEO independent launch logic and update launch rate to be a little lower

--- Global_SSN_ABM_LEOParams.m
+++ Global_SSN_ABM_LEOParams.m
@@ -1,7 +1,7 @@
 function envParams = Global_SSN_ABM_LEOParams(timeStep)
 
 envParams.leoSats            = 5000;                        % Total number of satellites in LEO
-envParams.leoLaunchRate      = 70.5/365.2425*timeStep;      % Total launch rate in LEO
+envParams.leoLaunchRate      = 50.5/365.2425*timeStep;      % Total launch rate in LEO
 envParams.leoVol             = 5.54e11*(1e9);               % LEO shell volume [km^3]
 envParams.initialSPD         = 2e-8*(1e-9);                 % Initial spatial debris density [debris objects / km^3]
 envParams.Asat               = 20;                          % Satellite cross-sectional area [m^2]
