b3f5fa5f0cd12c0e4259b71571f4b3e364f8e8fa
lwgreeriv <130858757+lwgreeriv@users.noreply.github.com>
2023-04-30 15:32:18 -0400

    Changed RQ2 DV to be tech multiplier
    -instead of just changing sensor capability, now nation 1's SC, sensor cost, and sensor con speed are all affected by a tech multiplier

--- Global_SSN_ABM_RQ2Driver.m
+++ Global_SSN_ABM_RQ2Driver.m
@@ -14,13 +14,13 @@ fclose all; close all; clearvars; clc; rng('default');
 numMC = 1;
 n_nations = 10;
 minGssnDQ = 0.6;
-nat1SensorCapability = 200:200:1000;
+nat1TechCapabilityScale = [0.5:0.25:3.0];
 timeStep = 8; % [days] %NOTE: this is also set independently at a lower level
 
 % initialize data storage
-minGssnTrackingSuccessProb = zeros(size(nat1SensorCapability));
-avgGssnMembership = zeros(size(nat1SensorCapability));
-all_gssa_models = cell(length(nat1SensorCapability),1);
+minGssnTrackingSuccessProb = zeros(size(nat1TechCapabilityScale));
+avgGssnMembership = zeros(size(nat1TechCapabilityScale));
+all_gssa_models = cell(length(nat1TechCapabilityScale),1);
 allNationParams(n_nations) = struct();
 
 % Econ params are always the same for each nation
@@ -40,49 +40,53 @@ for iNat = 1:n_nations
 end
 
 % iterate over nation 1 sensor capability DV
-for iSC = 1:length(nat1SensorCapability)
+for iTC = 1:length(nat1TechCapabilityScale)
     
-    % update nation 1's sensor capability for the current DV iteration
-    allNationParams(1).nationParams.sensor_capability = nat1SensorCapability(iSC);
+    curFactor = nat1TechCapabilityScale(iTC);
+    % update nation 1's tech capabilities for the current DV iteration
+    allNationParams(1).nationParams.sensor_capability = allNationParams(1).nationParams.sensor_capability*curFactor;
+    allNationParams(1).nationParams.sensor_const_speed = allNationParams(1).nationParams.sensor_const_speed*(1/curFactor);
+    allNationParams(1).nationParams.sensor_mfg_cost = allNationParams(1).nationParams.sensor_capability*(1/curFactor);
     
     % run MC simulation for current DV setting
     gssa_model = Global_SSN_ABM_MCDriver(numMC,allNationParams,econParams,minGssnDQ);
     gssnTrackingSuccessProb = gssa_model.gssn.data.tracking_capacity ./ gssa_model.leo_environment.data.totalDebris;
-    minGssnTrackingSuccessProb(iSC) = min(gssnTrackingSuccessProb);
-    avgGssnMembership(iSC) = mean(gssa_model.gssn.data.total_members_cum);
-    avgNation1GssnMembership(iSC,:) = mean(gssa_model.nations{1}.data.gssnMembership,1);
+    minGssnTrackingSuccessProb(iTC) = min(gssnTrackingSuccessProb);
+    avgGssnMembership(iTC) = mean(gssa_model.gssn.data.total_members_cum);
+    avgNation1GssnMembership(iTC,:) = mean(gssa_model.nations{1}.data.gssnMembership,1);
     
     % store gssa_model
-    all_gssa_models{iSC} = gssa_model;
+    all_gssa_models{iTC} = gssa_model;
 end
 
 % Min GSSN Tracking Success Probability vs Nation 1 Sensor Capability
 figure('Position',[600 400 720 420],'Color','w'); hold on; box on; grid on;
 yline(1.0,'g--','LineWidth',1.5);
 yline(1.2,'b--','LineWidth',1.5);
-plot(nat1SensorCapability,minGssnTrackingSuccessProb,'k*');
+plot(nat1TechCapabilityScale,minGssnTrackingSuccessProb,'k*');
 title('');
-xlabel('Nation 1 Sensor Capability','FontWeight','Bold');
+xlabel('Nation 1 Tech Multiplier','FontWeight','Bold');
 ylabel('Minimum GSSN Tracking Success Probability','Fontweight','Bold');
 legend({'100% Tracking Capacity Performance Requirement','120% Tracking Capacity Design Goal'},'Location','NorthEast');
 
 % Avg GSSN Membership vs Nation 1 Sensor Capability
 figure('Position',[600 400 720 420],'Color','w'); hold on; box on; grid on;
-plot(nat1SensorCapability,avgGssnMembership,'k*');
+plot(nat1TechCapabilityScale,avgGssnMembership,'k*');
 title('');
-xlabel('Nation 1 Sensor Capability','FontWeight','Bold');
+xlabel('Nation 1 Tech Multiplier','FontWeight','Bold');
 ylabel('Average GSSN Membership','Fontweight','Bold');
 
 % Average GSSN Membership OverTime for Nation 1 vs Nation 1 Sensor Capability                             
+plot_colors = distinguishable_colors(length(nat1TechCapabilityScale),'w');
 timeVec  = 0:timeStep:100*365.2425;    % Simulation time steps [days]
 xData = years(days(timeVec));
 figure('Position',[600 400 720 420],'Color','w'); hold on; box on; grid on;
-for iSC = 1:length(nat1SensorCapability)
-    plot(xData,avgNation1GssnMembership(iSC,:),'LineWidth',2);
+for iTC = 1:length(nat1TechCapabilityScale)
+    plot(xData,avgNation1GssnMembership(iTC,:),'LineWidth',2,'Color',plot_colors(iTC,:));
 end
 title('');
 xlabel('Time (Years)','FontWeight','Bold');
 ylabel('Average Nation 1 GSSN Membership','Fontweight','Bold');
 ax = gca; ax.XLim = round([xData(1) xData(end)]); ax.YLim = [0 1.1];
-lgdStr = strcat({'Sensor Capability '},strsplit(num2str(nat1SensorCapability)));
+lgdStr = strcat({'Tech Multiplier '},strsplit(num2str(nat1TechCapabilityScale)));
 legend(lgdStr,'location','northeastoutside');
