7b80206e309a348dc2f631d41cd8b5f83c44ccb8
jmandara <jmandara@purdue.edu>
2023-04-30 00:04:54 -0400

    Update Global_SSN_ABM_RQ1Driver.m
    - Added average GSSN membership plot

--- Global_SSN_ABM_RQ1Driver.m
+++ Global_SSN_ABM_RQ1Driver.m
@@ -13,12 +13,16 @@
 n_nations = 10;
 minGssnDQ = 0:0.2:1.0;
 minGssnTrackingSuccessProb = zeros(size(minGssnDQ));
+avgGssnMembership = zeros(size(minGssnDQ));
 all_gssa_models = cell(length(minGssnDQ),1);
 for iDQ = 1:length(minGssnDQ)
     gssa_model = Global_SSN_ABM_MCDriver(n_nations,minGssnDQ(iDQ));
-    all_gssa_models{iDQ} = gssa_model;
     gssnTrackingSuccessProb = gssa_model.gssn.data.tracking_capacity ./ gssa_model.leo_environment.data.totalDebris;
     minGssnTrackingSuccessProb(iDQ) = min(gssnTrackingSuccessProb);
+    avgGssnMembership(iDQ) = mean(gssa_model.gssn.data.total_members_cum);
+    
+    % store gssa_model
+    all_gssa_models{iDQ} = gssa_model;
 end
 
 % Min GSSN Tracking Success Probability vs Min GSSN Data Quality
@@ -30,3 +34,10 @@ title('');
 xlabel('Minimum GSSN Required Data Quality','FontWeight','Bold');
 ylabel('Minimum GSSN Tracking Success Probability','Fontweight','Bold');
 legend({'100% Tracking Capacity Performance Requirement','120% Tracking Capacity Design Goal'},'Location','NorthEast');
+
+% Avg GSSN Membership vs Min GSSN Data Quality
+figure('Position',[600 400 720 420],'Color','w'); hold on; box on; grid on;
+plot(minGssnDQ,avgGssnMembership,'k*');
+title('');
+xlabel('Minimum GSSN Required Data Quality','FontWeight','Bold');
+ylabel('Average GSSN Membership','Fontweight','Bold');
